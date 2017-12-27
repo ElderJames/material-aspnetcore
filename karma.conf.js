22c9ce4b54eeb5097ebde7a717e260f687400303
ElderJames <shunjiey@hotmail.com>
2017-12-28 01:55:37 +0800

    feat(): rename the src folder

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,12 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './src/test.ts', watched: false },
+      { pattern: './ClientApp/test.ts', watched: false },
       { pattern: './node_modules/hammerjs/hammer.min.js'},
       { pattern: './node_modules/@angular/material/prebuilt-themes/indigo-pink.css', included: true, watched: true },
     ],
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      './ClientApp/test.ts': ['@angular/cli']
     },
     mime: {
       'text/x-typescript': ['ts','tsx']
